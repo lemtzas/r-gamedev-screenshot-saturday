e9acc8cbb4ced9ea59da385c347aa233a7b00b30
Lemtzas <lemtzas@gmail.com>
2014-12-06 08:49:28 -0800

    regex fix

--- SSS_web_helper.rb
+++ SSS_web_helper.rb
@@ -137,7 +137,7 @@ module SSSProcessor
           icon = "fa fa-spinner"
         end
         # raw images
-        new_index = (text =~ /(https?:\/\/[A-Za-z0-9-\.]+?.*?\.(png|jpg|jpeg|gif))/i)
+        new_index = (text =~ /(https?:\/\/[A-Za-z0-9\-\.]+?.*?\.(png|jpg|jpeg|gif))/i)
         if new_index and new_index < earliest_index then
           earliest_index = new_index
           match_data = $~
