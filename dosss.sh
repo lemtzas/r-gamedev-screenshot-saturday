ad83b74edee82b2454c4d12017f501481a7fc34d
Lemtzas <lem@lemtzas.com>
2014-12-12 16:16:26 -0500

    do modified

--- dosss.sh
+++ dosss.sh
@@ -7,4 +7,5 @@ pwd
 git pull
 /home/lem/.rvm/wrappers/ruby-2.1.5/ruby SSS_web.rb
 /home/lem/.rvm/wrappers/ruby-2.1.5/ruby SSS_param.rb "LD31 Competition is Over! Post Games to Rate" "ld31.html"
+/home/lem/.rvm/wrappers/ruby-2.1.5/ruby SSS_param.rb "title:'End of 2014 Show us your game progress'" "eo2014.html"
 
