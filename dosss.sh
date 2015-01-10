48dace9314ac1a7da63723afcab05249a41f5ebc
Lemtzas <lem@lemtzas.com>
2015-01-10 01:17:25 -0500

    some fiddling

--- dosss.sh
+++ dosss.sh
@@ -6,5 +6,5 @@ cd $DIR
 pwd
 git pull
 /home/lem/.rvm/wrappers/ruby-2.1.5/ruby SSS_param.rb
-/home/lem/.rvm/wrappers/ruby-2.1.5/ruby SSS_param.rb -q "title:'End of 2014 Show us your game progress'" -o "eo2014.html"
+#/home/lem/.rvm/wrappers/ruby-2.1.5/ruby SSS_param.rb -q "title:'End of 2014 Show us your game progress'" -o "eo2014.html"
 
